61bb34aeadcd0529310975c6d199c0b9109a5a20
mbootsman <mbootsman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-14 11:33:27 +0000

    Minor changes to CSS becuase of changes in WordPress 3.3.
    git-svn-id: https://plugins.svn.wordpress.org/search-engines-blocked-in-header/tags/0.2.1@475313 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ search-engines-blocked-in-header.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Search Engines Blocked in Header
+Plugin URI: http://www.nostromo.nl/wordpress-plugins/search-engines-blocked-in-header
+Description: Display the 'Search Engines Blocked' notification in the Header in the Admin area.
+Author: Marcel Bootsman
+Version: 0.2.1
+Author URI: http://www.nostromo.nl
+*/
+
+add_action('in_admin_header', 'nostromo_sebih');
+function nostromo_sebih() {
+    if (!get_option('blog_public')) {
+        $title = apply_filters('privacy_on_link_title', __('Your site is asking search engines not to index its content'));
+        $content = apply_filters('privacy_on_link_text', __('Search Engines Blocked'));
+        
+        echo "<p style='position: absolute; top:39px; left: 330px;'><a title='$title' href='options-privacy.php'>$content</a></p>";
+    }
+}
+?>
