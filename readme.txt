9ad55e7b121c9bada1d4a9edc08e2319a9fdd7da
mbootsman <mbootsman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-16 19:11:40 +0000

    Updated description
    git-svn-id: https://plugins.svn.wordpress.org/search-engines-blocked-in-header/tags/0.5.1@894800 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -6,7 +6,7 @@ Requires at least: 3.2
 Tested up to: 3.9
 Stable tag: 0.5.1
 
-Display the 'Search Engines Blocked' notification in the WordPress Toolbar.
+Display the 'Search Engines Discouraged' (or any translation) notification in the WordPress Toolbar if blog_public option has been checked.
 
 == Description ==
 In WordPress 3.2 the 'Search Engines Blocked' notification was moved to the Right Now box. This plugin displays the 'Search Engines Blocked' notification in WordPress Toolbar.
--- search-engines-blocked-in-header.php
+++ search-engines-blocked-in-header.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Search Engines Blocked in Header
 Plugin URI: http://www.nostromo.nl/wordpress-plugins/search-engines-blocked-in-header
-Description: Display the 'Search Engines Discouraged' (or any translation) notification in the WordPress Toolbar if blog_public option has been checked .
+Description: Display the 'Search Engines Discouraged' (or any translation) notification in the WordPress Toolbar if blog_public option has been checked.
 Author: Marcel Bootsman
 Version: 0.5.1
 Author URI: http://www.nostromo.nl
